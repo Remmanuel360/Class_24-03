13c7a01bb301bc67c2b0f067492c05bf9540e03a
Remmanuel360 <137846673+Remmanuel360@users.noreply.github.com>
2026-04-20 02:04:21 +0000

    step 1

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,183 +1,79 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router'
 
-
-const equiposMap: Record<string, string> = {
-  "América de Cali SA": "america-de-cali",
-  "CA Bucaramanga": "atletico-bucaramanga",
-  "Club Atlético Nacional SA": "atletico-nacional",
-  "Club Deportes Tolima SA": "deportes-tolima",
-  "Asociación Deportivo Cali": "deportivo-cali",
-  "Deportivo Independiente Medellín": "independiente-medellin",
-  "Club Independiente Santa Fe": "independiente-santa-fe",
-  "CD Popular Junior FC SA": "junior",
-  "Millonarios FC": "millonarios",
-  "Once Caldas SA": "once-caldas",
-
-  "Internacional de Bogotá": "internacional-bogota",
-  "Club Llaneros SA": "llaneros",
-  "Águilas Doradas": "aguilas-doradas",
-  "Fortaleza FC": "fortaleza",
-  "Alianza FC": "alianza",
-  "Jaguares de Córdoba FC": "jaguares",
-  "Cúcuta Deportivo FC": "cucuta",
-  "Boyacá Chicó FC": "boyaca-chico",
-  "Deportivo Pereira FC": "pereira"
-};
-
-
-interface Ranking {
-  rank: number
-  contestantName: string
-  points: number
-  matchesPlayed: number
-}
-interface Estadistica {
-  position: number
+interface Personajes {
+  id: number
   name: string
-  value: number
-  appearances: number
-  contestantName: string
-  statName: string
+  status: string
+  species: string
+  gender: string
+  image: string
 }
-
-type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
-
 
 function Home() {
-  const [ranking, setRanking] = useState<Ranking[]>([])
+  const [personajes, setPersonajes] = useState<Personajes[]>([])
   const [title, setTitle] = useState('')
-  const [estadisticas, setEstadisticas] = useState<Estadistica[]>([])
-  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
-
-  const filtros: FiltroTipo[] = ['posiciones', 'goleador', 'asistencias', 'amarillas', 'atajadas'];
   const [busqueda, setBusqueda] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/${filtro}.json`)
+        const res = await fetch(`https://rickandmortyapi.com/api/character`)
         const data = await res.json()
 
-        if (filtro === 'posiciones') {
-          setRanking(data.standings[0].ranking)
-          setTitle(data.standings[0].competitionName)
-        } else {
-          setEstadisticas(data)
-        }
-
-        setRanking(data.standings[0].ranking)
-        setTitle(data.standings[0].competitionName)
-      } catch (error) {
+        setPersonajes(data.results)
+        setTitle("Personajes de Rick and Morty")
+      } catch (error) { 
         console.error('Error cargando datos:', error)
       }
     }
 
-
-
     fetchData()
-  }, [filtro])
+  }, [])
 
-    const rankingFiltrado = ranking.filter((equipo) =>
+  const personajesFiltrados = personajes.filter((p) =>
     busqueda.length < 3
-      ? true  // muestra todos si hay menos de 3 caracteres
-      : equipo.contestantName.toLowerCase().includes(busqueda.toLowerCase())
-  )
-
-  const estadisticasFiltradas = estadisticas.filter((jugador) =>
-    busqueda.length < 3
-      ? true  // muestra todos
-      : jugador.name.toLowerCase().includes(busqueda.toLowerCase()) ||
-        jugador.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+      ? true
+      : p.name.toLowerCase().includes(busqueda.toLowerCase())
   )
 
   return (
     <>
-      <div className="filtros">
-        {filtros.map((onestat) => (
-          <button
-            key={onestat}
-            onClick={() => setFiltro(onestat)}
-            className={filtro === onestat ? 'activo' : ''}
-          >
-            {onestat}
-          </button>
-        ))}
-      </div>
-
       <input
         type="text"
-        placeholder="Buscar..."
+        placeholder="Buscar personaje..."
         value={busqueda}
         onChange={(e) => setBusqueda(e.target.value)}
       />
 
       <div className="tabla-container">
         <h2>{title}</h2>
-        {filtro === 'posiciones' ? (
-          <table className="tabla-posiciones">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Equipo</th>
-                <th>PJ</th>
-                <th>Pts</th>
+
+        <table className="tabla-personajes">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Imagen</th>
+              <th>Nombre</th>
+              <th>Especie</th>
+              <th>Estado</th>
+              <th>Género</th>
+            </tr>
+          </thead>
+          <tbody>
+            {personajesFiltrados.map((p) => (
+              <tr key={p.id}>
+                <td>{p.id}</td>
+                <td>
+                  <img src={p.image} width="50" />
+                </td>
+                <td>{p.name}</td>
+                <td>{p.species}</td>
+                <td>{p.status}</td>
+                <td>{p.gender}</td>
               </tr>
-            </thead>
-            <tbody>
-              {rankingFiltrado.map((equipo) => (
-                <tr key={equipo.rank}
-                    className={
-                      busqueda.length >= 3 &&
-                      equipo.contestantName.toLowerCase().includes(busqueda.toLowerCase())
-                        ? 'resaltado'
-                        : ''
-                    }
-                >
-                  <td>{equipo.rank}</td>
-                  <td>
-                        <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
-                        {equipo.contestantName}
-                      </Link>
-                  </td>
-                  <td>{equipo.matchesPlayed}</td>
-                  <td>{equipo.points}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <table className="tabla-estadisticas">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Jugador</th>
-                <th>Equipo</th>
-                <th>PJ</th>
-                <th>{filtro}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {estadisticas.map((jugador, index) => (
-                <tr key={index}
-                    className={
-                      busqueda.length >= 3 &&
-                      (jugador.name.toLowerCase().includes(busqueda.toLowerCase()) ||
-                      jugador.contestantName.toLowerCase().includes(busqueda.toLowerCase()))
-                        ? 'resaltado'
-                        : ''
-                    }
-                >
-                  <td>{jugador.position}</td>
-                  <td>{jugador.name}</td>
-                  <td>{jugador.contestantName}</td>
-                  <td>{jugador.appearances}</td>
-                  <td>{jugador.value}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
